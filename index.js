09c2e8ddc7480f9a6689b8e7b72fc74e7a017de4
maxwellbenton <maxwellbenton@gmail.com>
2019-02-05 07:34:33 -0500

    updates tests

--- index.js
+++ index.js
@@ -8,39 +8,47 @@ const getPosts = () => {
   xhr.addEventListener( 'load', e => {
     JSON.parse( xhr.response )
       .map( post => {
+
         createPost( post )
+        post.comments.forEach( commentId => {
+
+          createEmptyComment( commentId, post.id )
+        } )
       } )
+
+    getComments()
   } )
   xhr.send();
 }
-const getComment = commentId => {
-  let xhr = new XMLHttpRequest();
-  xhr.open( 'GET', `http://localhost:3000/comments/${commentId}` );
-  xhr.addEventListener( 'load', e => {
-    createComment( JSON.parse( xhr.response ) )
-  } )
-  xhr.send();
-}
-const getComments = () => {
-  let xhr = new XMLHttpRequest();
-  xhr.open( 'GET', `http://localhost:3000/comments` );
-  xhr.addEventListener( 'load', e => {} )
-  xhr.send();
-}
+
 const createPost = post => {
   let section = document.createElement( 'section' )
   document.querySelector( 'main' )
     .appendChild( section )
   section.id = post.id
   section.innerHTML = `<h2>${post.title}</h2><h3><img src=${post.user.image}>${post.user.name}</h3><p>${post.body}</p>`
-  post.comments.forEach( comment => {
-    getComment( comment )
-  } )
 }
-const createComment = comment => {
+
+const createEmptyComment = ( commentId, postId ) => {
   let aside = document.createElement( 'aside' )
-  document.getElementById( `${comment.postId}` )
+  aside.id = commentId
+  document.getElementById( `${postId}` )
     .appendChild( aside )
-  aside.id = comment.id
+}
+
+const getComments = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open( 'GET', `http://localhost:3000/comments` );
+  xhr.addEventListener( 'load', e => {
+    JSON.parse( xhr.response )
+      .map( comment => {
+        updateCommentContent( comment )
+      } )
+  } )
+  xhr.send();
+}
+
+const updateCommentContent = comment => {
+  let aside = document.getElementById( `${comment.id}` )
   aside.innerHTML = `<h3><img src=${comment.user.image}>${comment.user.name}</h3><p>${comment.body}</p>`
 }
